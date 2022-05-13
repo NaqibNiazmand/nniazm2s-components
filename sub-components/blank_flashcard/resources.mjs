b14041f9f0c4a02b5ef99aa01d2411b3cd6d1bc6
NaqibNiazmand <naqib.niazmand@smaiil.inf.h-brs>
2022-05-14 00:10:45 +0200

    Test : gitHub.io

---
+++ sub-components/blank_flashcard/resources.mjs
@@ -0,0 +1,23 @@
+/**
+ * @overview Web component for multilingualism
+ * @author Naqib Niazmand <naqib.niazmand@smail.inf.h-brs.de>
+ * @license The MIT License (MIT)
+ * */
+
+/**
+ * german texts and labels
+ * @type {Object}
+ */
+export const de = {
+    "flashcard_name" : "Name",
+    "flashcard_id" : "ID",
+};
+
+/**
+ * english texts and labels
+ * @type {Object}
+ */
+export const en = {
+    "flashcard_name" : "Name",
+    "flashcard_id" : "ID",
+};
