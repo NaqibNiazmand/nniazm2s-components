181f4b7d8c55508f57c71e32e7029e0cae079d23
NaqibNiazmand <naqib.niazmand@smaiil.inf.h-brs>
2022-05-15 15:53:55 +0200

    style : update menu with own css

--- smart_memorize/versions/ccm.smart_memorize-1.0.1.js
+++ smart_memorize/versions/ccm.smart_memorize-1.0.1.js
@@ -105,6 +105,7 @@
                 var title_start_training_stack = this.lang.getValue() === "de" ? "Trainingsstapel starten" : "Start training stack";
                 this.menu && this.menu.start({
                     root: this.element.querySelector('main'),
+                    "css":  ["ccm.load", "./../smart_memorize/resources/menu.css"],
                     "selected": 1,
                     "data": {
                         "entries": [
