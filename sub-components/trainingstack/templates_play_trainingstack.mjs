a81c2ca07c0a0ba040034879b2b1f1d947843c93
NaqibNiazmand <naqib.niazmand@smaiil.inf.h-brs>
2022-06-05 01:31:01 +0200

    style : update start training stack buttons

--- sub-components/trainingstack/templates_play_trainingstack.mjs
+++ sub-components/trainingstack/templates_play_trainingstack.mjs
@@ -18,39 +18,13 @@ export function mainContent(app, events) {
                 <label id="space_top"></label>
                 <label data-lang="menu_left_check_translation">${app.text.menu_left_enter_translation}:</label>
                 <input type="text" name="menu_left_enter_translation_name" id="menu_left_enter_translation_input"/>
-                <style>
-                    .button {
-                        background-color: #4CAF50; /* Green */
-                        border: none;
-                        color: white;
-                        padding: 16px 32px;
-                        text-align: center;
-                        text-decoration: none;
-                        display: inline-block;
-                        font-size: 14pt;
-                        transition-duration: 0.4s;
-                        cursor: pointer;
-                    }
-                    .button1 {
-                        background-color: white;
-                        color: black;
-                        border: 2px solid #4CAF50;
-                        font-size: 14pt;
-                        margin-top: 10px;
-                    }
-                    .button1:hover {
-                        background-color: #4CAF50;
-                        color: white;
-                        font-size: 14pt;
-                    }
-                </style>
-                <button  class="button1" @click=${events.menu_left_check_translation_button} id="menu_left_check_translation_button" data-lang="menu_left_enter_translation">
+                <button  class="glow-on-hover" @click=${events.menu_left_check_translation_button} id="menu_left_check_translation_button" data-lang="menu_left_enter_translation">
                     ${app.text.menu_left_check_translation}
                 </button>
                 <label id="space_top"></label>
                 <label data-lang="menu_left_next_flashcard_label_part_1">${app.text.menu_left_next_flashcard_label_part_1}</label>
                 <label data-lang="menu_left_next_flashcard_label_part_2">${app.text.menu_left_next_flashcard_label_part_2}</label>
-                <button  class="button1" @click=${events.next_button} id="next" data-lang="menu_left_button_next">
+                <button  class="glow-on-hover" @click=${events.next_button} id="next" data-lang="menu_left_button_next">
                     ${app.text.menu_left_button_next}
                 </button>
             </div>
